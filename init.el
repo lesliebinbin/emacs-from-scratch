1fc551e214ea8a6fadb33d7a8eb3b2ecedc207ea
lesliebinbin <zhibin.huang@uqconnect.edu.au>
2025-01-11 10:29:57 +0800

    use ultra-scroll

--- init.el
+++ init.el
@@ -327,5 +327,11 @@
 
 (evilnc-default-hotkeys)
 
+(use-package ultra-scroll
+  :init
+  (setq scroll-conservatively 101 ; important!
+        scroll-margin 0) 
+  :config
+  (ultra-scroll-mode t))
 
 (toggle-frame-fullscreen)
